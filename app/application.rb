b1141190261867f25f8e5f6e06e16571ace61771
Jeff Benton <sikletz@gmail.com>
2019-04-27 03:58:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@card.length > 0
+      if @@cart.length > 0
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
