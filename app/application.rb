a895a6b9968b5ac033d62d84b4a12392a95c770f
Jeff Benton <sikletz@gmail.com>
2019-04-27 03:54:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-      @@cart << 
+      @@cart << req.params["q"]
     else
       resp.write "Path Not Found"
     end
